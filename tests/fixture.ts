e19e9d32a469fdf3720f1a6820888ce78bd6d89d
justYu2001 <jack4781012@gmail.com>
2023-02-18 23:55:41 +0800

    plasmo test

--- tests/fixture.ts
+++ tests/fixture.ts
@@ -39,7 +39,7 @@ export const it = test.extend<TestFixtures>({
     },
     page: async ({ browser }, use) => {
         const page = await browser.newPage();
-        page.setDefaultTimeout(60_000);
+        page.setDefaultTimeout(90_000);
         await use(page);
     },
     user: async ({}, use) => {
--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         deps: {
             inline: ["vitest-fixture"],
         },
-        testTimeout: 60_000,
+        testTimeout: 90_000,
         globals: true,
         environment: "jsdom",
         setupFiles: "./tests/setup.ts",
