4d64c6932aa6b1d7ac215baaed4ca13152bf8114
justYu2001 <jack4781012@gmail.com>
2023-02-19 10:27:01 +0800

    plasmo test

--- tests/signin-without-captcha.test.ts
+++ tests/signin-without-captcha.test.ts
@@ -5,22 +5,14 @@ import { it } from "./fixture";
 
 describe.concurrent("Sign in without a captcha", () => {
     it("should redirect to the index page of the portal app after the user signs in successfully", async ({ env, page }) => {
-        console.info("before goto");
         await page.goto("https://app.ntut.edu.tw/index.do");
-        console.info("after goto");
 
-        console.info("before fill");
         await page.locator("#student-id").fill(env.studentID);
         await page.locator("#password").fill(env.password);
-        console.info("after fill");
 
-        console.info("before click");
-        await page.getByRole("button",  { name: "登入" }).click();
-        console.info("after click");
+        await page.locator("button[type='submit']").click();
 
-        console.info("before redirect");
         await page.waitForURL("https://app.ntut.edu.tw/myPortal.do*");
-        console.info("after redirect");
 
         const portalAppUrlRegex = new RegExp("^https://app\.ntut\.edu\.tw/myPortal\.do.*$");
 
@@ -33,7 +25,7 @@ describe.concurrent("Sign in without a captcha", () => {
         await page.locator("#student-id").fill("a");
         await page.locator("#password").fill(env.password);
 
-        await page.getByRole("button",  { name: "登入" }).click();
+        await page.locator("button[type='submit']").click();
         await page.waitForURL("https://app.ntut.edu.tw/login.do");
 
         expect(page.url()).toBe("https://app.ntut.edu.tw/login.do");
@@ -50,7 +42,7 @@ describe.concurrent("Sign in without a captcha", () => {
         await page.locator("#student-id").fill(env.studentID);
         await page.locator("#password").fill("a");
 
-        await page.getByRole("button",  { name: "登入" }).click();
+        await page.locator("button[type='submit']").click();
         await page.waitForURL("https://app.ntut.edu.tw/login.do");
 
         expect(page.url()).toBe("https://app.ntut.edu.tw/login.do");
